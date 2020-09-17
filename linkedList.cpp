f797ece388b8326dffb8e79b8ad74be8e6909a3e
anushkawakankar <anushka.wakankar@gmail.com>
2020-09-17 22:20:43 +0530

    working ll

--- linkedList.cpp
+++ linkedList.cpp
@@ -3,38 +3,117 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-  int val;
-  struct Node* next;
-}node;
+class Node{
+public:
+  int key;
+  Node* next;
+};
 
-void addToList(struct Node* head,, int val)
+void addToList(Node** head, int val)
 {
-  struct Node* temp = head;
-  while(temp != NULL)
+      Node* new_node = new Node();
+
+      Node *last = *head;
+
+      new_node->key = val;
+      new_node->next = NULL;
+      if (*head == NULL)
+      {
+          *head = new_node;
+          return;
+      }
+
+      while (last->next != NULL)
+          last = last->next;
+
+      last->next = new_node;
+      return;
+}
+
+void insertAfter(Node** head, int val, int after)
+{
+  Node* new_node = new Node();
+  Node* last = *head;
+  new_node->key = val;
+  while(last->key != after)
+        last = last->next;
+  new_node->next = last->next;
+  last->next = new_node;
+}
+
+void printList(Node* n)
+{
+    while (n != NULL) {
+        cout << n->key << " ";
+        n = n->next;
+    }
+}
+
+void deleteNode(Node** head, int val){
+
+  Node* last = *head;
+  if(last != NULL && last->key == val)
   {
-      temp = temp->next;
+    *head = last->next;
+    free(last);
+    return;
   }
-  temp 
+  Node* prev;
+  while(last != NULL && last->key != val)
+  {
+    prev = last;
+    last = last->next;
+  }
+
+  if(last == NULL) return;
+  prev->next = last->next;
+  free(last);
+}
+
+Node* reverseLL(Node** head)
+{
+  Node* current = *head;
+  Node* prev = NULL;
+  Node* next = NULL;
+  while(current != NULL)
+  {
+    next = current->next;
+    current->next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
 }
 
 int main()
 {
-  struct Node* head = NULL;
-  int c;
-  cin>>c;
-  if(c==1)
-  {
-    int a;
-    cin>>a;
-    while(a>=0)
-    {
-      addToList(head,a);
-      cin>>a;
-    }
+  int n;
+  cout<<"Enter no. of values and values"<<endl;
+  cin>>n;
+  int first;
+  cin>>first;
+  Node* head = NULL;
+  head = new Node();
+  head->key = first;
+  head->next = NULL;
+  int in;
+  for(int i = 1;i<n;i++){
+    cin>>in;
+    addToList(&head,in);
   }
-  if(c==2)
-  {
-    printList(head);
-  }
+  printList(head);
+  cout<<endl;
+  cout<<"Enter number + which node to add after"<<endl;
+  cin>>in;
+  int after;
+  cin>>after;
+  insertAfter(&head,in,after);
+  printList(head);
+  cout<<"\nEnter node to be deleted"<<endl;
+  cin>>in;
+  deleteNode(&head,in);
+  printList(head);
+  cout<<"\nReversed:"<<endl;
+  head = reverseLL(&head);
+  printList(head);
 }
